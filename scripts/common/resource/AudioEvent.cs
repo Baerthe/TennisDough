6154e7afc23a026bacf7b767222f552f6917eb8a
AuctorisMors <GitHub@Baerthe.com>
2026-02-04 20:33:30 -0800

    Add background shader and audio event resources; update theme and main scene for new assets

--- scripts/common/resource/AudioEvent.cs
+++ scripts/common/resource/AudioEvent.cs
@@ -4,6 +4,7 @@ using Godot;
 /// <summary>
 /// A resource representing an audio event with an associated audio clip.
 /// </summary>
+[GlobalClass]
 public sealed partial class AudioEvent : Resource
 {
     [Export] public AudioStream Clip { get; private set; }
